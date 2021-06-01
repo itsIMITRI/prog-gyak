739afa1d6dc6ebe65147cb78c26e3e070fcbf6dd
Imre Varga <vargaimi990330@gmail.com>
2021-06-01 16:54:05 +0200

    binary tree

---
+++ java2/day5_gyak1/src/hu/flowacademy/MyRunnable.java
@@ -0,0 +1,8 @@
+package hu.flowacademy;
+
+public class MyRunnable implements Runnable {
+
+    public void run(){
+        System.out.println("MyRunnable running");
+    }
+}
---
+++ java2/day5_gyak1/src/hu/flowacademy/MyThread.java
@@ -0,0 +1,8 @@
+package hu.flowacademy;
+
+public class MyThread extends Thread{
+
+    public void run(){
+        System.out.println("Thread fut");
+    }
+}
